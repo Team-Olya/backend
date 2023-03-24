4fb5f516c898f655087bf8b5b3a04b36a048d3a9
sonnka <sonyakazanceva1331@gmail.com>
2023-03-24 21:53:53 +0200

    Reversed the talent list

--- src/main/java/com/teamolha/talantino/repository/TalentRepository.java
+++ src/main/java/com/teamolha/talantino/repository/TalentRepository.java
@@ -1,13 +1,16 @@
 package com.teamolha.talantino.repository;
 
 import com.teamolha.talantino.model.entity.Talent;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TalentRepository extends JpaRepository<Talent, Long> {
     Optional<Talent> findByEmailIgnoreCase(String email);
     boolean existsByEmailIgnoreCase(String email);
+    List<Talent> findAllByOrderByIdDesc(Pageable pageable);
 }
