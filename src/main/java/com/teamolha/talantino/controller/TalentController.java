95f604701c12aebeac7246d873f9b8c043ba5b7c
Vladyslav Liubchyk <lyuben1337@gmail.com>
2023-03-27 15:35:55 +0200

    fix: removed symbol

--- src/main/java/com/teamolha/talantino/controller/TalentController.java
+++ src/main/java/com/teamolha/talantino/controller/TalentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 
-@RestControllerg
+@RestController
 @AllArgsConstructor
 public class TalentController {
 
