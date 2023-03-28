0b15900aeef02fb4823b644ffd550c538e49b152
Vladyslav Liubchyk <lyuben1337@gmail.com>
2023-03-28 20:21:34 +0200

    fix cors for all requsts v4

--- src/main/java/com/teamolha/talantino/config/WebSecurityConfig.java
+++ src/main/java/com/teamolha/talantino/config/WebSecurityConfig.java
@@ -75,6 +75,7 @@ public class WebSecurityConfig {
                         .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
                 );
 
+        http.cors();
 
         return http.build();
     }
