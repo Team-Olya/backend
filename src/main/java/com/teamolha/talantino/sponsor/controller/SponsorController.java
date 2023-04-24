73ae522475f856e13446c802c098b415744cffe0
shdwraze <dedmoped1997@gmail.com>
2023-04-24 18:07:53 +0300

    feature: sponsor can add funds

--- src/main/java/com/teamolha/talantino/sponsor/controller/SponsorController.java
+++ src/main/java/com/teamolha/talantino/sponsor/controller/SponsorController.java
@@ -1,12 +1,23 @@
 package com.teamolha.talantino.sponsor.controller;
 
+import com.teamolha.talantino.sponsor.model.response.SponsorProfileResponse;
+import com.teamolha.talantino.sponsor.service.SponsorService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class SponsorController {
 
+    private SponsorService sponsorService;
 
+    @PostMapping("/sponsors/balance")
+    public SponsorProfileResponse addFunds(Authentication auth,
+                                           @RequestParam(value = "amount") Long amount) {
+        return sponsorService.addFunds(auth, amount);
+    }
 }
--- src/main/java/com/teamolha/talantino/sponsor/service/impl/SponsorServiceImpl.java
+++ src/main/java/com/teamolha/talantino/sponsor/service/impl/SponsorServiceImpl.java
@@ -1,12 +1,15 @@
 package com.teamolha.talantino.sponsor.service.impl;
 
 import com.teamolha.talantino.general.config.Roles;
+import com.teamolha.talantino.sponsor.mapper.SponsorMapper;
 import com.teamolha.talantino.sponsor.model.entity.Sponsor;
+import com.teamolha.talantino.sponsor.model.response.SponsorProfileResponse;
 import com.teamolha.talantino.sponsor.repository.SponsorRepository;
 import com.teamolha.talantino.sponsor.service.SponsorService;
 import com.teamolha.talantino.talent.repository.TalentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,12 +20,13 @@ import java.util.List;
 @AllArgsConstructor
 public class SponsorServiceImpl implements SponsorService {
     private PasswordEncoder encoder;
+    private SponsorMapper mapper;
     private TalentRepository talentRepository;
     private SponsorRepository sponsorRepository;
 
     @Override
     public void register(String email, String password, String name, String surname) {
-        if(talentRepository.existsByEmailIgnoreCase(email) || sponsorRepository.existsByEmailIgnoreCase(email)) {
+        if (talentRepository.existsByEmailIgnoreCase(email) || sponsorRepository.existsByEmailIgnoreCase(email)) {
             throw new ResponseStatusException(HttpStatus.CONFLICT,
                     email + " is already occupied!"
             );
@@ -37,5 +41,18 @@ public class SponsorServiceImpl implements SponsorService {
                         .authorities(List.of(Roles.SPONSOR.name()))
                         .build()
         );
+    }
+
+    @Override
+    public SponsorProfileResponse addFunds(Authentication auth, Long amount) {
+        var sponsor = sponsorRepository.findByEmailIgnoreCase(auth.getName()).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.FORBIDDEN, "Only sponsor can add funds"));
+        if (amount > 1000) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You can add a maximum of 1000 per transaction");
+        } else if (amount < 1) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "We can't pay to your card, sorry");
+        }
+        sponsor.setBalance(sponsor.getBalance() + amount);
+        return mapper.toSponsorProfileResponse(sponsorRepository.save(sponsor));
     }
 }
