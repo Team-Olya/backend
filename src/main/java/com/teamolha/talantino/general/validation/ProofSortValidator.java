c4205d66ac18924964601acbe5ac0e269257fad3
shdwraze <73839791+shdwraze@users.noreply.github.com>
2023-05-08 17:43:43 +0300

    Update ProofSortValidator.java

--- src/main/java/com/teamolha/talantino/general/validation/ProofSortValidator.java
+++ src/main/java/com/teamolha/talantino/general/validation/ProofSortValidator.java
@@ -10,10 +10,10 @@ public class ProofSortValidator implements ConstraintValidator<ProofSort, String
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        boolean valid = value.equals("date") || value.equals("label");
+        boolean valid = value.equals("date") || value.equals("title");
         if (!valid) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                    "It can be sorted only by date or label");
+                    "It can be sorted only by date or title");
         }
         return true;
     }
